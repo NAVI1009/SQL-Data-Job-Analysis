a0acf471f7a29ce0a448c2ec991ceb5cea3474ac
NAVI1009 <navdeep02005@gmail.com>
2026-07-18 19:56:13 +0530

    Update 1_create_database.sql

--- SQL_Load/1_create_database.sql
+++ SQL_Load/1_create_database.sql
@@ -1,3 +1,17 @@
+/*
+===========================================
+Database Setup
+===========================================
+
+Objective:
+Create the PostgreSQL database used for the
+Data Analyst Job Market Analysis project.
+
+Purpose:
+This database stores all job postings,
+company information, and skills data required
+for the analysis.
+*/
 CREATE DATABASE sql_course;
 
 -- DROP DATABASE IF EXISTS sql_course;
